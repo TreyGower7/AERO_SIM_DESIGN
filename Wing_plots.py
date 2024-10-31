497be7e22d5ce841de6d0050b513f654057b2fb0
Trey Gower <gowertrey@yahoo.com>
2024-10-31 12:07:24 -0500

    Clean up

--- Wing_plots.py
+++ Wing_plots.py
@@ -3,10 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def get_data():
-    
+    xls = pd.ExcelFile("https://github.com/TreyGower7/AERO_SIM_DESIGN/blob/main/wing/Aerodynamic_Testing_Wing.xlsx")
+    AOAN2 = pd.read_excel(xls, 'Wing - AOA - -2.5')
+    AOA0 = pd.read_excel(xls, 'Wing - AOA - 0')
+    AOA2 = pd.read_excel(xls, 'Wing - AOA - 2.5')
+    AOA5 = pd.read_excel(xls, 'Wing - AOA - 5')
+    AOA10 = pd.read_excel(xls, 'Wing - AOA - 10')
+    AOA12 = pd.read_excel(xls, 'Wing - AOA - 15')
+
+    return [AOAN2, AOA0, AOA2, AOA5,AOA10,AOA12]
+
 
 def main():
-
+    df = get_data()
+    print(df[0])
 
 if __name__ == "main":
     main()
